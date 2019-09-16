b51a34cfbfa4b4f05b6bf3a9d9ad42d00b4b1aec
Sarah Pryor <you@example.com>
2019-09-16 15:00:40 -0400

    Do cherokee-hare math stuff

--- assets/javascript/cherokee-hare.js
+++ assets/javascript/cherokee-hare.js
@@ -1,6 +1,12 @@
+let startingPopulation = 50;
+let birthRate = 0.10;
+let numberOfWeeks = 6;
 
+let weeklyGain = startingPopulation * birthRate;
+console.log(weeklyGain);
 
-
+let currentPopulation = (weeklyGain * numberOfWeeks) + startingPopulation;
+console.log(currentPopulation);
 
 
 
